8cec258861daa8efa60bd2aef96e8b0f83de2d71
Ziyan Wang <zywong159@gmail.com>
2016-12-04 15:15:45 -0500

    Simple Sketch Completed

--- sketch.py
+++ sketch.py
@@ -1,27 +1,17 @@
 import sys
 from murmurhash import *
 from MinHashHeap import *
-
 from HashList import *
 
 class Sketch:
 
 	# init sketch
-	def __init__(self, kmerSize = 10, maxSizeOfHashList = 10):
+	def __init__(self, kmerSize = 21, maxSizeOfHashList = 1000):
 		self.kmerSize = kmerSize
-
 		self.hashList = HashList(maxSizeOfHashList)
-
-		self.hashes = []
-		self.maxHashSize = 10
-		self.maxHashValue = 0
 	
-
 	def addToHashes(self,value):
 		self.hashList.add(value)
-
-
-
 
 	def getHashesFromFile(self, filename):
 		input_file = open(filename,'r')
@@ -36,16 +26,26 @@ class Sketch:
 			kmer = seq[i:i+self.kmerSize]
 			self.addToHashes(murmur64(kmer))
 
-
-
+	def writeSketch(self,filename):
+		output_file = open(filename,'w')
+		hl = self.hashList.getList()
+		for i in hl:
+			s = str(i) + '\n'
+			output_file.write(s)
+		output_file.close()
 
 # for test 
 def test():
-	mySketch = Sketch()
+	mySketch1 = Sketch()
 	filename = 'genome1.fna'
-	mySketch.getHashesFromFile(filename)
-	print mySketch.hashList.getList()
-	
+	mySketch1.getHashesFromFile(filename)
+	mySketch1.writeSketch('genome1.msh')
+
+	mySketch2 = Sketch()
+	filename = 'genome2.fna'
+	mySketch2.getHashesFromFile(filename)
+	mySketch2.writeSketch('genome2.msh')
+
 
 if __name__ == '__main__':
 	test()
