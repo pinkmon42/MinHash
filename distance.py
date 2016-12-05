a7e67e55ccea52f4fe2ec58744edfbbc8361da6f
Damien <iamyichaodu@gmail.com>
2016-12-05 16:10:59 -0500

    distance v2

--- distance.py
+++ distance.py
@@ -1,145 +1,103 @@
-import sketch
+from __future__ import division
+from sketch import *
 import math
-# sketchRefNew = 1
-# sketchQueryNew = 2
-# indexRefNew = 3
-# indexQueryNew = 4
-# pairCountNew = 5
-# parametersNew = 6
-# maxDistanceNew = 7
-# maxPValueNew = 8
 
-class CompareInput:
+def factorial(n): 
+    if n < 2: return 1
+    return reduce(lambda x, y: x*y, xrange(2, int(n)+1))
 
-    def __init__(self, sketchRefNew, sketchQueryNew, indexRefNew, indexQueryNew, pairCountNew, parametersNew, maxDistanceNew, maxPValueNew):
-        # self.__dict__.update(data)
-        self.sketchRef = sketchRefNew
-        self.sketchQuery = sketchQueryNew
-        self.indexRef = indexRefNew
-        self.indexQuery = indexQueryNew
-        self.pairCount = pairCountNew
-        self.parameters = parametersNew
-        self.maxDistance = maxDistanceNew
-        self.maxPValue = maxPValueNew
+def prob(s, p, n):
+    x = 1.0 - p
 
+    a = n - s
+    b = s + 1
+
+    c = a + b - 1
+
+    prob = 0.0
+
+    for j in xrange(a, c + 1):
+        prob += factorial(c) / (factorial(j)*factorial(c-j)) \
+                * x**j * (1 - x)**(c-j)
+
+    return prob
 
 
-
-# i = CompareInput(sketchRefNew, sketchQueryNew, indexRefNew,
-#     indexQueryNew, pairCountNew, parametersNew, maxDistanceNew, maxPValueNew)
-# print(i.sketchRef)
+def hashLessThan(hash1, hash2):
+    return hash1 < hash2
 
 
-# a = 1
-# b = 2
-# c = 3
-# d = 4
-# e = 5
+# def compare(input):
+#     sketchRef = input.sketchRef
+#     sketchQuery = input.sketchQuery
 
-class PairOutput:
-        def __init__(self, pairCountNew):
-            pass
-            # numer
-            # denom
-            # distance
-            # pValue
-            # ifPass
+#     output = CompareOutput(input.sketchRef, input.sketchQuery, input.indexRef, input.indexQuery, input.pairCount)
+#     if sketchQuery.getMinHashesPerWindow() < sketchRef.getMinHashesPerWindow():
+#         sketchSize = sketchQuery.getMinHashesPerWindow()
+#     else:
+#         sketchSize = sketchRef.getMinHashesPerWindow()
 
+#     i = input.indexQuery
+#     j = input.indexRef
 
-class CompareOutput:
+#     k = 0
+#     while k < input.pairCount and i < sketchQuery.getReferenceCount()
+#         compareSketches(output.pairs[k], sketchRef.getReference(j), sketchQuery.getReference(i), sketchSize, sketchRef.getKmerSize(), sketchRef.getKmerSpace(), input.maxDistance, input.maxPValue)
+#         j += 1
 
-    def __init__(self, sketchRefNew, sketchQueryNew, indexRefNew, indexQueryNew, pairCountNew):
-        self.sketchRef = sketchRefNew
-        self.sketchQuery = sketchQueryNew
-        self.indexRef = indexRefNew
-        self.indexQuery = indexQueryNew
-        self.pairCount = pairCountNew
-        self.pairs = PairOutput(pairCountNew)
+#         if j ==  sketchRef.getReferenceCount():
+#             j = 0
+#             i += 1
 
-    
+#         k += 1
 
-# j = CompareOutput(a,b,c,d,e)
-# print(j.sketchRef)
-
-
-def CommandDistance():
-    pass
-
-
-def run():
-    pass
-
-#CompareOutput *output bool table
-def __writeOutput(output, table):
-    i = output.indexQuery
-    j = output.indexRef
-    k = 0
-    while k < output.pairCount and i < output.sketchQuery.getReferenceCount():
-        pair = output.pairs[k]
-        if table and j == False:
-            print(output.sketchQuery.getReference(i).name)
-        
-        if table == True:
-            print('\t')
-            if (pair.ifPass == True):
-                print(pair.distance)
-        elif pair.ifPass == True:
-            print(output.sketchRef.getReference(j).name + "\t" + output.sketchQuery.getReference(i).name + "\t" + pair.distance + "\t" + pair.pValue + "\t" + pair.numer + "\t" + pair.denom, end="")
-
-        j += 1
-
-        if j == output.sketchRef.getReferenceCount():
-            if table == True:
-                print()
-
-            j = 0
-            i += 1
-        
-        k += 1
-
-#CompareInput input
-def compare(input):
-    sketchRef = input.sketchRef
-    sketchQuery = input.sketchQuery
-
-    output = CompareOutput(input.sketchRef, input.sketchQuery, input.indexRef, input.indexQuery, input.pairCount)
-    if sketchQuery.getMinHashesPerWindow() < sketchRef.getMinHashesPerWindow():
-        sketchSize = sketchQuery.getMinHashesPerWindow()
-    else:
-        sketchSize = sketchRef.getMinHashesPerWindow()
-
-    i = input.indexQuery
-    j = input.indexRef
-
-    k = 0
-    while k < input.pairCount and i < sketchQuery.getReferenceCount()
-        compareSketches(output.pairs[k], sketchRef.getReference(j), sketchQuery.getReference(i), sketchSize, sketchRef.getKmerSize(), sketchRef.getKmerSpace(), input.maxDistance, input.maxPValue)
-        j += 1
-
-        if j ==  sketchRef.getReferenceCount():
-            j = 0
-            i += 1
-
-        k += 1
-
-    return output
+#     return output
 
 #CompareOutput::Pairoutput output
-def compareSketches(output, refRef, refQry, sketchSize, kmerSize, kmerSpace, maxDistance, maxPValue):
+# def compareSketches(output, refRef, refQry, sketchSize, kmerSize, kmerSpace, maxDistance, maxPValue):
+def compareSketches(refRefName, refQryName):
+    
+    refRef = open(refRefName,'r')
+    refQry = open(refQryName,'r')
+
+    sketchCount1 = 0
+    sketchCount2 = 0
+    hashesSortedRef = []
+    hashesSortedQry = []
+
+    for ln in refRef:
+        sketchCount1 += 1
+        hashesSortedRef.append(ln.strip())
+
+    for ln in refQry:
+        sketchCount2 += 1
+        hashesSortedQry.append(ln.strip())
+
+    if sketchCount1 < sketchCount2:
+        sketchSize = sketchCount1
+    else:
+        sketchSize = sketchCount2
+
+    kmerSize = 21
+    kmerSpace = 4 ** kmerSize
+
     i = 0
     j = 0
     common = 0
     denom = 0
     #hashlist
-    hashesSortedRef = refRef.hashesSorted
-    hashesSortedQry = refQry.hashesSorted
+    # hashesSortedRef = refRef.hashesSorted
+    # hashesSortedQry = refQry.hashesSorted
+    
 
-    output.ifPass = False
+    # output.ifPass = False
 
-    while denom < sketchSize and i < hashesSortedRef.size() and j < hashesSortedQry.size():
-        if hashLessThan(hashesSortedRef.at(i), hashesSortedQry.at(j), hashesSortedRef.get64()):
+    while denom < sketchSize and i < sketchCount1 and j < sketchCount2:
+        # if hashLessThan(hashesSortedRef.at(i), hashesSortedQry.at(j), hashesSortedRef.get64()):
+        if hashLessThan(hashesSortedRef[i], hashesSortedQry[j]):
             i += 1
-        elif hashLessThan(hashesSortedQry.at(j), hashesSortedRef.at(i), hashesSortedRef.get64()):
+        # elif hashLessThan(hashesSortedQry.at(j), hashesSortedRef.at(i), hashesSortedRef.get64()):
+        elif hashLessThan(hashesSortedQry[j], hashesSortedRef[i]):
             j += 1
         else:
             i += 1
@@ -149,16 +107,18 @@ def compareSketches(output, refRef, refQry, sketchSize, kmerSize, kmerSpace, max
         denom += 1
 
     if denom < sketchSize:
-        if i < hashesSortedRef.size():
-            denom += hashesSortedRef.size() - i
+        if i < sketchCount1:
+            denom += sketchCount1 - i
 
-        if j < hashesSortedQry.size():
-            denom += hashesSortedQry.size() - j
+        if j < sketchCount2:
+            denom += sketchCount2 - j
 
         if denom > sketchSize:
             denom = sketchSize
 
-    distance
+    # print common
+    # print denom
+    distance = 0
     #notice the round up here!!
     jaccard = common / denom
 
@@ -167,35 +127,50 @@ def compareSketches(output, refRef, refQry, sketchSize, kmerSize, kmerSpace, max
     elif common == 0:
         distance = 1
     else:
-        #what does 1. here mean??
-        distance = -math.log(2 * jaccard / (1. + jaccard)) /kmerSize
+        # print jaccard
+        #what does 1. here mean?
+        distance = -log(2 * jaccard / (1 + jaccard), math.e) / kmerSize
 
-    if distance > maxDistance:
-        return
+    # if distance > maxDistance:
+    #     return
 
+    # output.numer = common
+    # output.denom = denom
+    # output.distance = distance
+    # output.pValue = pValue(common, refRef.length, refQry.length,kmerSpace, denom)
 
-    output.numer = common
-    output.denom = denom
-    output.distance = distance
-    output.pValue(common, refRef.length, refQry.length,kmerSpace, denom)
+    # if output.pValue > maxPValue:
+    #     return
 
-    if output.pValue > maxPValue:
-        return
-
-    output.ifPass = True
-
-
+    # output.ifPass = True
+    p = 0
+    p = pValue(common, sketchCount1, sketchCount2, kmerSpace, denom)
+    print 'distance:', distance
+    print 'pValue:', p
 
 def pValue(x, lengthRef, lengthQuery, kmerSpace, sketchSize):
-    #what does 1. here mean
+    #x is the number of shared kmers between sketches of size s if genome1 and genome2
     if x == 0:
-        return 1.
+        return 1
 
-    pX = 1. / (1. + kmerSpace / lengthRef)
-    pY = 1. / (1. + kmerSpace / lengthQuery)
+    #r1 kmerSpace |sigama|k
+    pX = 1 / (1 + kmerSpace / lengthRef)
+    #r2
+    pY = 1 / (1 + kmerSpace / lengthQuery)
 
+    #j_r
     r = pX * pY / (pX + pY - pX * pY)
-    # M = kmerSpace * (pX + pY) / (1. + r);
-    # return gsl_cdf_hypergeometric_Q(x - 1, r * M, M - r * M, sketchSize);
+    # print pX
+    # print pY
+    # print x-1
+    # print r
+    # print sketchSize
 
-    
+    return prob(x-1, r, sketchSize)
+
+def test():
+    compareSketches('genome1.msh', 'genome2.msh')
+
+
+if __name__ == '__main__':
+    test()
